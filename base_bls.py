f4d0420c8e3a12a7aa376fefa7cb551404a5f73e
alphaanfre <1012501615@qq.com>
2021-11-17 16:40:40 +0800

    do messidor exp

--- base_bls.py
+++ base_bls.py
@@ -51,13 +51,13 @@ class BaseBls:
         self.each_window_min.append(np.min(x, axis=0))
         return (x - self.each_window_min[index]) / self.dist_maxmin[index]
 
-    def get_enhance_weight(self):
-        if self.N1 * self.N2 >= self.N3:
+    def get_enhance_weight(self, N1, N2, N3):
+        if N1 * N2 >= N3:
             np.random.seed(67797325)
-            return LA.orth(2 * np.random.randn(self.N2 * self.N1 + 1, self.N3)) - 1
+            return LA.orth(2 * np.random.randn(N2 * N1 + 1, N3)) - 1
         else:
             np.random.seed(67797325)
-            return LA.orth(2 * np.random.randn(self.N2 * self.N1 + 1, self.N3).T - 1).T
+            return LA.orth(2 * np.random.randn(N2 * N1 + 1, N3).T - 1).T
         
     def pinv(self, A):
         return np.mat(self.C*np.eye(A.shape[1])+A.T.dot(A)).I.dot(A.T)
@@ -68,33 +68,49 @@ class BaseBls:
         return W
 
     def train_forward(self, train_x, train_y):
+        time1_s = time.time()
         train_x = preprocessing.scale(train_x, axis=1)
+        #train_x = np.load("messidor.npy")
+        time1_e = time.time()
+        print("time1 durations is ", time1_e - time1_s)
+        #np.save("messidor.npy", train_x)
         x_bias = np.hstack([train_x, 0.1 * np.ones((train_x.shape[0], 1))])
-        mapping_layer_output = np.zeros([train_x.shape[0], self.N2 * self.N1])
+        self.mapping_layer_output = np.zeros([train_x.shape[0], self.N2 * self.N1])
 
         for i in range(self.N2):
+            print("windows : ", i)
+            time2_s = time.time()
             np.random.seed(i)
             feature_weight = 2 * np.random.randn(train_x.shape[1]+1, self.N1) - 1
             feature_window = np.dot(x_bias, feature_weight)
+            time2_e = time.time()
+            print("time2 durations is ", time2_e - time2_s)
             scaler1 = preprocessing.MinMaxScaler(feature_range=(0, 1)).fit(feature_window)
             preprocess_feature = scaler1.transform(feature_window)
             sparse_feature_weight = self.sparse_autoencoder(preprocess_feature, x_bias)
             self.feature_weight_list.append(sparse_feature_weight)
+            time3_s = time.time()
             feature_node_output = np.dot(x_bias, sparse_feature_weight)
+            time3_e = time.time()
+            print("time3 durations is ", time3_e - time3_s)
             feature_node_output = self.standardization(feature_node_output, i)
-            mapping_layer_output[:, self.N1*i:self.N1*(i+1)] = feature_node_output
+            self.mapping_layer_output[:, self.N1*i:self.N1*(i+1)] = feature_node_output
 
-        enhance_layer_input = np.hstack([mapping_layer_output, 0.1 * np.ones((mapping_layer_output.shape[0], 1))])
-        self.enhance_weight = self.get_enhance_weight()
+        time4_s = time.time()
+        enhance_layer_input = np.hstack([self.mapping_layer_output, 0.1 * np.ones((self.mapping_layer_output.shape[0], 1))])
+        self.enhance_weight = self.get_enhance_weight(self.N1, self.N2, self.N3)
         temp_enhance_output = np.dot(enhance_layer_input, self.enhance_weight)
         self.shrink_para = self.S / np.max(temp_enhance_output)
         enhance_layer_output = self.tansig(temp_enhance_output * self.shrink_para)
-
-        output_layer_input = np.hstack([mapping_layer_output, enhance_layer_output])
-        self.output_weight = self.get_output_weight(output_layer_input, train_y)
+        time4_e = time.time()
+        print("time4 durations is ", time4_e - time4_s)
+        self.output_layer_input = np.hstack([self.mapping_layer_output, enhance_layer_output])
+        time5_s = time.time()
+        self.output_weight = self.get_output_weight(self.output_layer_input, train_y)
         
-        output_train = np.dot(output_layer_input, self.output_weight)
-
+        output_train = np.dot(self.output_layer_input, self.output_weight)
+        time5_e = time.time()
+        print("time5 durations is ", time5_e - time5_s)
         return output_train
 
     def eval_forward(self, test_x):
@@ -121,3 +137,16 @@ class BaseBls:
             return self.train_forward(x, y)
         else:
             return self.eval_forward(x)
+
+    # def add_enhanceNode(self, iters, add_num):
+    #     self.add_shrink_para = []
+    #     for i in range(iters):
+    #         new_enhance_weight = self.get_enhance_weight()
+
+    #     temp_enhance_output = np.dot(self.mapping_layer_output, new_enhance_weight)
+    #     self.shrink_para = self.S / np.max(temp_enhance_output)
+    #     add_enhance_layer_output = self.tansig(temp_enhance_output * self.shrink_para)        
+        
+    #     temp_output_layer_input = np.hstack([self.output_layer_input, add_enhance_layer_output])
+
+    #     D = 
--- demo.py
+++ demo.py
@@ -2,24 +2,8 @@ import numpy as np
 import scipy.io as scio
 from base_bls import BaseBls
 import time
-
-def show_accuracy(predictLabel, Label): 
-    count = 0
-    label_1 = np.zeros(Label.shape[0])
-    predlabel = []
-    label_1 = Label.argmax(axis=1)
-    predlabel = predictLabel.argmax(axis=1)
-    for j in list(range(Label.shape[0])):
-        if label_1[j] == predlabel[j]:
-            count += 1
-    return (round(count/len(Label),5))
-
-dataFile = './mnist.mat'
-data = scio.loadmat(dataFile)
-traindata = np.double(data['train_x']/255)
-trainlabel = np.double(data['train_y'])
-testdata = np.double(data['test_x']/255)
-testlabel = np.double(data['test_y'])
+from utils import show_mnist_accuracy
+from keras.preprocessing.image import ImageDataGenerator
 
 N1 = 10
 N2 = 10
@@ -27,21 +11,128 @@ N3 = 500
 s = 0.8
 C = 2**-30
 
-print("------BLS_BASE------")
-bls = BaseBls(N1, N2, N3, s, C)
-start_time = time.time()
-train_out = bls.forward(traindata, trainlabel)
-end_time = time.time()
-train_time = end_time - start_time
-train_acc = show_accuracy(train_out, trainlabel)
-print('Training accurate is' ,train_acc*100,'%')
-print('Training time is ',train_time,'s')
+def test_mnist(file_path=None):
+    dataFile = file_path
+    data = scio.loadmat(dataFile)
+    traindata = np.double(data['train_x']/255)
+    trainlabel = np.double(data['train_y'])
+    testdata = np.double(data['test_x']/255)
+    testlabel = np.double(data['test_y'])
 
-bls.eval()
-start_time = time.time()
-predict_out = bls.forward(testdata)
-end_time = time.time()
-test_time = end_time - start_time
-test_acc = show_accuracy(predict_out, testlabel)
-print('Testing accurate is' ,test_acc * 100,'%')
-print('Testing time is ',test_time,'s')
+    print("------BLS_BASE------")
+    bls = BaseBls(N1, N2, N3, s, C)
+    start_time = time.time()
+    train_out = bls.forward(traindata, trainlabel)
+    end_time = time.time()
+    train_time = end_time - start_time
+    train_acc = show_mnist_accuracy(train_out, trainlabel)
+    print('Training accurate is' ,train_acc*100,'%')
+    print('Training time is ',train_time,'s')
+
+    bls.eval()
+    start_time = time.time()
+    predict_out = bls.forward(testdata)
+    end_time = time.time()
+    test_time = end_time - start_time
+    test_acc = show_mnist_accuracy(predict_out, testlabel)
+    print('Testing accurate is' ,test_acc * 100,'%')
+    print('Testing time is ',test_time,'s')
+    return
+
+
+def test_messidor(dataset="messidor", image_size=512, batch_size=16):
+    dataParam={'messidor': [957,243,2,'./datasets/messidor/train','./datasets/messidor/test'],
+               'kaggle': [30000,5126,5,'./datasets/kaggle/train','./datasets/kaggle/valid'],
+               'DDR':   [9851,2503,5,'./datasets/DDR/train','./datasets/DDR/valid']}
+    
+    train_num,valid_num,classes,train_dir,test_dir = dataParam[dataset]
+    
+    train=ImageDataGenerator(horizontal_flip=True,vertical_flip=True,rotation_range=90)          
+    valid = ImageDataGenerator()
+    train_data=train.flow_from_directory(train_dir,
+                                         target_size=(image_size,image_size),
+                                         shuffle = True,
+                                         batch_size=batch_size)
+    valid_data=valid.flow_from_directory(test_dir,
+                                         target_size=(image_size,image_size),
+                                         shuffle = False,
+                                         batch_size=batch_size)
+
+    train_x = np.zeros([train_num, image_size*image_size*3])
+    train_y = np.zeros([train_num, classes])
+
+    test_x = np.zeros([valid_num, image_size*image_size*3])
+    test_y = np.zeros([valid_num, classes])
+
+    count = 0
+    for batch_data in train_data:
+        count += 1
+        print("now is batch : ", count)
+        pre_trainx = batch_data[0]
+        pre_trainy = batch_data[1]
+
+        start_index = (count - 1) * batch_size
+        if count != 60:
+            pre_trainx = np.resize(
+                pre_trainx, (batch_size, image_size*image_size*3))
+            end_index = count * batch_size
+        elif count == 60:
+            pre_trainx = np.resize(
+                pre_trainx, (13, image_size*image_size*3))
+            end_index = start_index + 13
+        train_x[start_index : end_index, :] = pre_trainx
+        train_y[start_index : end_index, :] = pre_trainy
+
+        if count == 60:
+            break
+    
+    count = 0
+    for batch_data in valid_data:
+        count += 1
+        print("val now is batch : ", count)
+        pre_testx = batch_data[0]
+        pre_testy = batch_data[1]
+
+        start_index = (count - 1) * batch_size
+        if count != 16:
+            pre_testx=np.resize(
+                pre_testx, (batch_size, image_size*image_size*3))
+            end_index = count * batch_size
+        elif count == 16:
+            pre_testx=np.resize(
+                pre_testx, (3, image_size*image_size*3))
+            end_index = start_index + 3
+        test_x[start_index: end_index, :]=pre_testx
+        test_y[start_index : end_index, :] = pre_testy
+
+        if count == 16:
+            break
+
+    traindata = np.double(train_x/255)
+    trainlabel = np.double(train_y)
+    testdata = np.double(test_x/255)
+    testlabel = np.double(test_y)
+
+    print("------BLS_BASE------")
+    bls = BaseBls(N1, N2, N3, s, C)
+    start_time = time.time()
+    train_out = bls.forward(traindata, trainlabel)
+    end_time = time.time()
+    train_time = end_time - start_time
+    train_acc = show_mnist_accuracy(train_out, trainlabel)
+    print('Training accurate is', train_acc*100, '%')
+    print('Training time is ', train_time, 's')
+
+    bls.eval()
+    start_time = time.time()
+    predict_out = bls.forward(testdata)
+    end_time = time.time()
+    test_time = end_time - start_time
+    test_acc = show_mnist_accuracy(predict_out, testlabel)
+    print('Testing accurate is', test_acc * 100, '%')
+    print('Testing time is ', test_time, 's')
+    return
+
+if __name__ == "__main__":
+    # test_mnist("./datasets/mnist.mat")
+    test_messidor()
