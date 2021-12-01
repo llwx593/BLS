3b75ce04efd85097fd1563563cea612534f37331
alphaanfre <1012501615@qq.com>
2021-12-01 17:50:55 +0800

    fix some small problem

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import numpy as np
 
 def show_mnist_accuracy(predictLabel, Label): 
@@ -10,17 +9,4 @@ def show_mnist_accuracy(predictLabel, Label):
     for j in list(range(Label.shape[0])):
         if label_1[j] == predlabel[j]:
             count += 1
-=======
-import numpy as np
-
-def show_mnist_accuracy(predictLabel, Label): 
-    count = 0
-    label_1 = np.zeros(Label.shape[0])
-    predlabel = []
-    label_1 = Label.argmax(axis=1)
-    predlabel = predictLabel.argmax(axis=1)
-    for j in list(range(Label.shape[0])):
-        if label_1[j] == predlabel[j]:
-            count += 1
->>>>>>> refs/remotes/origin/master
     return (round(count/len(Label),5))
